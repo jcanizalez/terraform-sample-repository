e2baa12bea7028cbb8a5077de8f375295ebad772
Javier Canizalez <javier-canizalez@outlook.com>
2024-09-10 14:50:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ resource "time_sleep" "wait2" {
     create_duration = var.time
 }
 
+resource "time_sleep" "wait3" {
+    create_duration = var.time
+}
+
+
 
 output "create_duration" {
   value = time_sleep.wait.create_duration
